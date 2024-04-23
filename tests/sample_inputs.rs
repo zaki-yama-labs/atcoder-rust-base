0508b49ef2953023863ebb6a8f28dbc0dd5934fb
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2024-04-24 04:44:59 +0900

    テスト用ファイルを微修正

--- tests/sample_inputs.rs
+++ tests/sample_inputs.rs
@@ -7,42 +7,41 @@ fn sample1() {
     let testdir = TestDir::new(BIN, "");
     let output = testdir
         .cmd()
-        .output_with_stdin(r#"2
-3 1 2
-6 1 1
-"#)
+        // change here
+        .output_with_stdin(r#" "#)
         .tee_output()
         .expect_success();
-    assert_eq!(output.stdout_str(), "Yes\n");
+    // change here
+    assert_eq!(output.stdout_str(), "\n");
     assert!(output.stderr_str().is_empty());
 }
 
 #[test]
+#[ignore]
 fn sample2() {
     let testdir = TestDir::new(BIN, "");
     let output = testdir
         .cmd()
-        .output_with_stdin(r#"1
-2 100 100
-"#)
+        // change here
+        .output_with_stdin(r#" "#)
         .tee_output()
         .expect_success();
-    assert_eq!(output.stdout_str(), "No\n");
+    // change here
+    assert_eq!(output.stdout_str(), "\n");
     assert!(output.stderr_str().is_empty());
 }
 
 #[test]
+#[ignore]
 fn sample3() {
     let testdir = TestDir::new(BIN, "");
     let output = testdir
         .cmd()
-        .output_with_stdin(r#"2
-5 1 1
-100 1 1
-"#)
+        // change here
+        .output_with_stdin(r#" "#)
         .tee_output()
         .expect_success();
-    assert_eq!(output.stdout_str(), "No\n");
+    // change here
+    assert_eq!(output.stdout_str(), "\n");
     assert!(output.stderr_str().is_empty());
 }
-
